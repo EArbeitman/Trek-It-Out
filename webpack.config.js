955a80e1eb21453fb3a5ec0004d8bc1b04cdf10f
Evan Arbeitman <evanarbeitman@gmail.com>
2017-06-21 19:17:41 -0400

    Recovering from webpack fiasco

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,11 @@ module.exports = {
         // Webpack will only process files in our app folder. This avoids processing
         // node modules and server files unnecessarily
         include: /app/,
-        loader: "babel",
+        loader: "babel-loader",
         query: {
           // These are the specific transformations we'll be using.
           presets: ["react", "es2015"]
-        },
-         // { test: /\.css$/, loader: "style-loader!css-loader" }
+        }
       }
     ]
   },
