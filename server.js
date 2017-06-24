a27e0a6a8e79d12d88a58f21da751e9ff0489ef6
Evan Arbeitman <evanarbeitman@gmail.com>
2017-06-24 11:26:14 -0400

    Began integration database connection functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require('mongoose');
+var express 			= require('express');
+var bodyParser 			= require("body-parser");
+var logger 				= require("morgan");
+var mongoose 			= require('mongoose');
 var path 				= require('path');
 var cookieParser 		= require('cookie-parser');
 var exphbs 				= require('express-handlebars');
@@ -12,8 +12,14 @@ var passport 			= require('passport');
 var LocalStrategy 		= require('passport-local').Strategy;
 var mongo 				= require('mongodb');
 
+// Import User model
 var User = require('./models/User.js')
 mongoose.Promise = Promise;
+
+var db = mongoose.connection;
+
+//var routes = require('./routes/index');
+var users  = require('./routes/users');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -28,13 +34,15 @@ app.use(express.static("./public"));
 
 //--------------------------- MONGOOSE ---------------------------
 
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI)
-// } else {
-  mongoose.connect('mongodb://heroku_kqxcxvjb:v013pehj9jabhajnh039cjelc4@ds011734.mlab.com:11734/heroku_kqxcxvjb');
-// }
+ // if (process.env.MONGODB_URI) {
+ //   mongoose.connect(process.env.MONGODB_URI)
+ // } else {
+ //  mongoose.connect('mongodb://heroku_kqxcxvjb:v013pehj9jabhajnh039cjelc4@ds011734.mlab.com:11734/heroku_kqxcxvjb');
+ // }
 
-var db = mongoose.connection;
+ //local db
+mongoose.connect('mongodb: //localhost/trekItOutDev');
+
 
 // Show any Mongoose errors
 db.on("error", function(error) {
@@ -47,8 +55,6 @@ db.once("open", function() {
 });
 
 //--------------------------- ROUTES ---------------------------
-
-
 
 //route for react HTML page
 app.get('/', function(req,res) {
