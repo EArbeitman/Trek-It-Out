7a6272cea9a2a5b64519efb1c6ef3cacf3fe664e
Evan Arbeitman <evanarbeitman@gmail.com>
2017-07-02 14:44:29 -0400

    Convert stops data type to array

--- routes/tours.js
+++ routes/tours.js
@@ -30,7 +30,7 @@ router.post("/create", function(req, res) {
     else {
     	//res.sendStatus(200);
       //Find our user and push the new note id into the User's notes array
-      User.findOneAndUpdate({}, { $push: { "tours_created": doc._id } }, { new: true }, function(err, newdoc) {
+      User.findOneAndUpdate({}, { $push: { "tours_created": doc._id }, tours_stops : doc.tours_stops }, { new: true }, function(err, newdoc) {
         // Send any errors to the browser
         if (err) {
           res.send(err);
@@ -56,6 +56,24 @@ router.get("/search", function(req, res){
     // Or send the doc to the browser
     else {
       res.send(doc);
+      //return doc;
+    }
+  });
+
+});
+
+router.get("/stops", function(req, res){
+
+  //console.log('tour params ' + req.body.category);
+
+  Tour.find({_id : "5959368d2f37e0047d94ed83"}, function(error, doc) {
+    // Send any errors to the browser
+    if (error) {
+      res.send(error);
+    }
+    // Or send the doc to the browser
+    else {
+      res.send(doc[0].toObject().tours_stops);
       //return doc;
     }
   });
